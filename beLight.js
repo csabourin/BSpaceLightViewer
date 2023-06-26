d01e794f20b42457cf58cb3957c8c2a3cac8f9cc
Christian Sabourin <christian.sabourin@canada.ca>
2023-06-26 16:21:09 -0400

    First commit

---
+++ beLight.js
@@ -0,0 +1,184 @@
+﻿const express = require("express");
+const fs = require("fs");
+const xml2js = require("xml2js");
+const parser = new xml2js.Parser();
+const ejs = require("ejs");
+const AdmZip = require("adm-zip");
+const path = require("path");
+const os = require("os");
+const multer = require("multer");
+
+const app = express();
+let firstResourceId = null;
+const storage = multer.diskStorage({
+	destination: function (req, file, cb) {
+		cb(null, "./packages/");
+	},
+	filename: function (req, file, cb) {
+		cb(null, file.originalname);
+	},
+});
+
+const upload = multer({ storage: storage });
+app.set("view engine", "ejs");
+
+let manifest = null;
+const tempDir = path.join(os.tmpdir(), "ims_package");
+let imsPackagePath = null;
+
+function getPackages() {
+	return new Promise((resolve, reject) => {
+		fs.readdir("./packages", (err, files) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(files);
+			}
+		});
+	});
+}
+const readPackage = (packagePath) => {
+    return new Promise((resolve, reject) => {
+        imsPackagePath = path.join("./packages/", packagePath); // replace with your IMS package path
+
+        const zip = new AdmZip(imsPackagePath);
+        zip.extractAllTo(tempDir, /*overwrite*/ true);
+
+        import("strip-bom").then((stripBom) => {
+            fs.readFile(
+                path.join(tempDir, "imsmanifest.xml"),
+                "utf8",
+                function (err, data) {
+                    if (err) {
+                        console.error(err);
+                        reject(err);
+                        return;
+                    }
+
+                    parser.parseString(stripBom.default(data), function (err, result) {
+                        if (err) {
+                            console.error(err);
+                            reject(err);
+                            return;
+                        }
+
+                        const manifestItems =
+                            result.manifest.organizations[0].organization[0].item;
+                        const resources = result.manifest.resources[0].resource;
+
+                        manifest = manifestItems.map((item) => {
+                            const moduleTitle = item.title[0];
+                            const items = item.item
+                                ? item.item
+                                        .map((i) => {
+                                            const itemResource = resources.find(
+                                                (r) => r.$.identifier === i.$.identifierref
+                                            );
+                                            if (
+                                                itemResource &&
+                                                itemResource.$["d2l_2p0:material_type"] !== "content"
+                                            ) {
+                                                return null;
+                                            }
+                                            const href = itemResource ? itemResource.$.href : null;
+                                            return {
+                                                title: i.title[0],
+                                                href,
+                                            };
+                                        })
+                                        .filter((i) => i !== null)
+                                : []; // filter out null values
+
+                            return {
+                                moduleTitle,
+                                items,
+                            };
+                        });
+                        resolve();
+                    });
+                }
+            );
+        });
+    });
+};
+
+
+app.get("/", async (req, res) => {
+	try {
+		const packageFiles = await getPackages();
+		res.render("index", { packageFiles });
+	} catch (error) {
+		console.error(error);
+		res.status(500).send("Error reading packages");
+	}
+});
+
+app.post("/upload", upload.single("zipFile"), (req, res) => {});
+app.get('/load/:filename', async (req, res) => {
+    try {
+        const filename = req.params.filename;
+        // Sanitize filename before use
+        // ...
+
+        // Read the package and load resources
+        await readPackage(filename);
+
+        // Ensure the manifest is loaded and the first module has items
+        if (!manifest || !manifest[0] || !manifest[0].items || !manifest[0].items[0]) {
+            console.error('Invalid manifest');
+            res.status(500).send('Invalid manifest');
+            return;
+        }
+
+        // Get the first resource id from the manifest
+        const firstResourceId = manifest[0].items[0].title;
+
+        // Redirect to the resource page
+        res.redirect(`/resource/${encodeURIComponent(firstResourceId)}`);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error processing the package');
+    }
+});
+
+
+app.use("/resource", express.static(tempDir));
+app.use("/shared", express.static("shared"));
+app.get("/resource/:id", (req, res) => {
+	const id = req.params.id;
+	const moduleIndex = manifest.findIndex((m) =>
+		m.items.find((i) => i.title === id)
+	);
+	const module = manifest[moduleIndex];
+
+	if (!module) {
+		res.status(404).send("Resource not found");
+		return;
+	}
+
+	const resourceIndex = module.items.findIndex((i) => i.title === id);
+	const resource = module.items[resourceIndex];
+	let prevResource = module.items[resourceIndex - 1];
+	let nextResource = module.items[resourceIndex + 1];
+
+	// If the nextResource does not exist in the current module,
+	// fetch the first item from the next module as the nextResource
+	if (!nextResource && manifest[moduleIndex + 1]) {
+		nextResource = manifest[moduleIndex + 1].items[0];
+	}
+	if (!prevResource && manifest[moduleIndex - 1]) {
+		prevResource = manifest[moduleIndex - 1].items[0];
+	}
+
+	res.render("resource", {
+		resource,
+		prevResource,
+		nextResource,
+		manifest, // pass the manifest to the view
+		currentPage: id,
+	});
+});
+
+app.listen(3000, () => {
+	console.log("App listening on port 3000!");
+});
---
+++ readme.md
@@ -0,0 +1,56 @@
+﻿
+# IMS Package Viewer
+
+This is an application for viewing IMS packages. It is a web-based solution written in Node.js and uses Express.js as a web server.
+
+## Features
+
+- Upload and extract zip files containing IMS packages.
+- Display the content of the IMS package in a web page.
+- Navigate between resources in the IMS package.
+
+## Prerequisites
+
+Before you begin, ensure you have met the following requirements:
+
+- You have installed Node.js and npm. You can download and install them from [here](https://nodejs.org/en/download/).
+
+## Installing the Application
+
+To install the application, follow these steps:
+
+1. Clone the repository:
+
+https://github.com/csabourin/BSpaceLightViewer.git
+
+2. Install the dependencies:
+
+```
+cd BSpaceLightViewer
+npm install
+```
+
+## Running the Application
+
+To run the application, navigate to the application directory and run the following command:
+
+```
+node beLight.js
+```
+
+Now open your web browser and navigate to `http://localhost:3000/` to see the application running.
+
+## Usage
+
+1. On the main page, click on the upload button to upload an IMS package (zip file).
+2. After the package is uploaded, the contents will be displayed on the page.
+3. You can navigate between resources using the 'Previous' and 'Next' buttons.
+
+## Contributing
+
+If you want to contribute to this project, please feel free to fork the repository and submit a pull request.
+
+## License
+
+This project uses the following license: Creative Commons wit attribution.
+
